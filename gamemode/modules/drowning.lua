f3aa541dfb4bf0fa07cae1ef4a8d68dd78e277ad
Bleck <OverlordBleck@gmail.com>
2020-06-01 18:58:55 -0500

    Simple implementation of the drowning mechanic

---
+++ gamemode/modules/drowning.lua
@@ -0,0 +1,47 @@
+local firstSubmergedTime = {}
+local lastDrownTick = {}
+local WATER_SUBMERGED = 3
+
+-- How many seconds before a submerged player is considered drowning
+local drowningThreshold = 45
+local drowningTickDelay = 2
+local drowningDamage = 5
+
+local function isDrowning( player )
+    return ( CurTime() - firstSubmergedTime[player:SteamID()] ) >= drowningThreshold
+end
+
+local function canDrownDamage( player )
+    return ( CurTime() - lastDrownTick[player:SteamID()] ) >= drowningTickDelay
+end
+
+local function takeDrownDamage( player )
+    if canDrownDamage( player ) then return end
+
+    local dmg = DamageInfo()
+    dmg:SetDamage( drowningDamage )
+    dmg:SetDamageType( DMG_DROWN )
+
+    player:TakeDamageInfo( dmg )
+end
+
+
+local function drowningCheck()
+    for _, ply in pairs( player.GetHumans() ) do
+        local ply_sid = ply:SteamID()
+
+        if ply:WaterLevel() == WATER_SUBMERGED then
+            if not firstSubmergedTime[ply_sid] then
+                firstSubmergedTime[ply_sid] = CurTime()
+            end
+
+            if isDrowning( ply ) then
+                takeDrownDamage( ply )
+            end
+        else
+            firstSubmergedTime[ply_sid] = nil
+        end
+    end
+end
+
+hook.Add( "Think", "RVR_Check_Drowning", drowningCheck )
