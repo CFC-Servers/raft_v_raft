aa7346b282bcf7039b64f094c44fa4cccb66b355
Samuel Williams <33094920+samuelWilliams99@users.noreply.github.com>
2020-06-02 16:27:47 +0100

    Add includes

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -2,3 +2,5 @@ AddCSLuaFile( "cl_init.lua" )
 AddCSLuaFile( "shared.lua" )
 
 include( "shared.lua" )
+
+include( "modules/inventory/sv_inventory.lua" )
--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -2,3 +2,4 @@ GM.Name = "Raft v Raft"
 GM.Author = ""
 GM.Website = "https://github.com/cfc-servers/raft_v_raft"
 
+include( "modules/items/sh_items.lua" )
