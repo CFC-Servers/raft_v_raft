a97e0e69b32d030125e6b1290e7615cd01257034
PLally <11771225+PLally@users.noreply.github.com>
2020-06-02 15:35:31 +0000

    includeShared function

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -3,5 +3,4 @@ AddCSLuaFile( "shared.lua" )
 
 include( "shared.lua" )
 
-AddCSLuaFile( "modules/hunger/sh_hunger.lua" )
 include( "modules/hunger/sv_hunger.lua" )
--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -2,4 +2,10 @@ GM.Name = "Raft v Raft"
 GM.Author = ""
 GM.Website = "https://github.com/cfc-servers/raft_v_raft"
 
-include( "modules/hunger/sh_hunger.lua")
+local function includeShared( f )
+    AddCSLuaFile( f )
+    include( f )
+end
+
+includeShared( "modules/hunger/sh_hunger.lua" )
+
