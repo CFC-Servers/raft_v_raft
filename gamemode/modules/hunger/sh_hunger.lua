c078606d737d3b48a5e315e94bb8fab3417444ac
PLally <11771225+PLally@users.noreply.github.com>
2020-06-02 03:14:06 +0000

    add thirst 💦

--- gamemode/modules/hunger/sh_hunger.lua
+++ gamemode/modules/hunger/sh_hunger.lua
@@ -10,7 +10,23 @@ end
 
 function PlayerMeta:AddHunger(n)
     local hunger = self:GetNWInt("rvr_hunger")
-    hunger = math.max( hunger, 0 )
-    self:SetNWInt("rvr_hunger", hunger + n)
+    hunger = math.max( hunger+n, 0 )
+    self:SetNWInt("rvr_hunger", hunger)
 end
+
+
+function PlayerMeta:GetThirst()
+    return self:GetNWInt("rvr_thirst")
+end
+
+function PlayerMeta:SetThirst()
+    self:SetNWInt("rvr_thirst")
+end
+
+function PlayerMeta:AddThirst(n)
+    local thirst = self:GetNWInt("rvr_thirst")
+    thirst = math.max( thirst + n, 0 )
+    self:SetNWInt("rvr_thirst", thirst)
+end
+
 
