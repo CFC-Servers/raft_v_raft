a5f95b203c274deb7f3ff6d962c7c6f9b7b303b7
PLally <11771225+PLally@users.noreply.github.com>
2020-06-02 15:31:49 +0000

    hunger -> food, thirst -> water, AddCSLua hunger file

--- gamemode/modules/hunger/sv_hunger.lua
+++ gamemode/modules/hunger/sv_hunger.lua
@@ -1,25 +1,25 @@
-local MAX_HUNGER = 100 -- TODO extract this into a global config table
-local MAX_THIRST = 100
+local MAX_FOOD = 100 -- TODO extract this into a global config table
+local MAX_WATER = 100
 
 local DELAY = 5
 
-local THIRST_LOSS_AMOUNT = -1
-local HUNGER_LOSS_AMOUNT = -1
+local WATER_LOSS_AMOUNT = -1
+local FOOD_LOSS_AMOUNT = -1
 local DAMAGE_AMOUNT = 10
 
 hook.Add( "PlayerSpawn", "RaftVRaft_HungerReset", function( ply, transition )
-    ply:SetHunger( MAX_HUNGER )
-    ply:SetThirst( MAX_THIRST )
+    ply:SetFood( MAX_HUNGER )
+    ply:SetWater( MAX_WATER )
 end )
 
-timer.Create( "rvr_hunger_subtract", HUNGER_DELAY, 0, function()
+timer.Create( "rvr_hunger_subtract", DELAY, 0, function()
     for _, ply in pairs( player.GetHumans() ) do
         
-        ply:AddHunger( HUNGER_LOSS_AMOUNT )
-        ply:AddThirst( THIRST_LOSS_AMOUNT )
+        ply:AddFood( FOOD_LOSS_AMOUNT )
+        ply:AddWater( WATER_LOSS_AMOUNT )
 
         -- TODO seperate timer
-        if ply:GetHunger() <= 0 then
+        if ply:GetFood() <= 0 then
             ply:TakeDamage( DAMAGE_AMOUNT )
         end
     end
