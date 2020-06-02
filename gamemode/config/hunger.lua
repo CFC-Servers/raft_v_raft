01df408987d868a3908c856a47457ccbaef6d144
PLally <11771225+PLally@users.noreply.github.com>
2020-06-02 17:25:39 +0000

    seperate  Hunger config table

--- gamemode/config/hunger.lua
+++ gamemode/config/hunger.lua
@@ -1,12 +1,13 @@
 GM.Config = GM.Config or  {}
 
-local Config = GM.Config
+GM.Config.Hunger = {}
+local Config = GM.Config.Hunger
 
 Config.MAX_FOOD = 100
 Config.MAX_WATER = 100
-Config.HUNGER_DELAY = 5
+Config.DELAY = 5
 Config.FOOD_LOSS_AMOUNT = -1
 Config.WATER_LOSS_AMOUNT = -1
-Config.HUNGER_DAMAGE_AMOUNT = 10
+Config.DAMAGE_AMOUNT = 10
 
 
