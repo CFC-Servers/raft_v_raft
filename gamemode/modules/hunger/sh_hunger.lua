6fcb6b5a78b4fd49854ab12c67fe4c227d879208
PLally <11771225+PLally@users.noreply.github.com>
2020-06-02 15:54:01 +0000

    seperate config file

--- gamemode/modules/hunger/sh_hunger.lua
+++ gamemode/modules/hunger/sh_hunger.lua
@@ -1,32 +1,32 @@
 local PlayerMeta = FindMetaTable( "Player" )
 
 function PlayerMeta:GetFood()
-    return self:GetNWInt("rvr_food")
+    return self:GetNWInt( "rvr_food" )
 end
 
-function PlayerMeta:SetFood(n)
-    self:SetNWInt("rvr_food", n)
+function PlayerMeta:SetFood( n )
+    self:SetNWInt( "rvr_food", n )
 end
 
-function PlayerMeta:AddFood(n)
-    local food = self:GetNWInt("rvr_food")
-    food = math.max( food+n, 0 )
-    self:SetNWInt("rvr_food", food)
+function PlayerMeta:AddFood( n )
+    local food = self:GetNWInt( "rvr_food" )
+    food = math.Clamp( food + n, 0, GM.Config.MAX_FOOD )
+    self:SetNWInt( "rvr_food", food )
 end
 
 
 function PlayerMeta:GetWater()
-    return self:GetNWInt("rvr_water")
+    return self:GetNWInt( "rvr_water" )
 end
 
 function PlayerMeta:SetWater()
-    self:SetNWInt("rvr_water")
+    self:SetNWInt( "rvr_water" )
 end
 
 function PlayerMeta:AddWater(n)
-    local water = self:GetNWInt("rvr_water")
-    water = math.max( thirst + n, 0 )
-    self:SetNWInt("rvr_water", thirst)
+    local water = self:GetNWInt( "rvr_water" )
+    water = math.Clamp( thirst + n, 0, GM.Config.MAX_WATER )
+    self:SetNWInt( "rvr_water", thirst )
 end
 
 
