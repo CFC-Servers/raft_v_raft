1b21d59b4792961eed4c253bf9daa09871c7a167
PLally <11771225+PLally@users.noreply.github.com>
2020-06-02 03:14:06 +0000

    add thirst 💦

--- gamemode/modules/hunger/sv_hunger.lua
+++ gamemode/modules/hunger/sv_hunger.lua
@@ -1,19 +1,26 @@
 local MAX_HUNGER = 100 -- TODO extract this into a global config table
-local HUNGER_DELAY = 5
-local HUNGER_LOSS_AMOUNT = -1
-local HUNGER_DAMAGE_AMOUNT = 10
+local MAX_THIRST = 100
 
-hook.Add("PlayerSpawn", "RaftVRaft_HungerReset", function( ply, transition )
+local DELAY = 5
+
+local THIRST_LOSS_AMOUNT = -1
+local HUNGER_LOSS_AMOUNT = -1
+local DAMAGE_AMOUNT = 10
+
+hook.Add( "PlayerSpawn", "RaftVRaft_HungerReset", function( ply, transition )
     ply:SetHunger( MAX_HUNGER )
-end)
+    ply:SetThirst( MAX_THIRST )
+end )
 
 timer.Create( "rvr_hunger_subtract", HUNGER_DELAY, 0, function()
     for _, ply in pairs( player.GetHumans() ) do
-        ply:AddHunger(HUNGER_LOSS_AMOUNT)
-    
+        
+        ply:AddHunger( HUNGER_LOSS_AMOUNT )
+        ply:AddThirst( THIRST_LOSS_AMOUNT )
+
         -- TODO seperate timer
         if ply:GetHunger() <= 0 then
-            ply:TakeDamage( HUNGER_DAMAGE_AMOUNT )
+            ply:TakeDamage( DAMAGE_AMOUNT )
         end
     end
 end )
