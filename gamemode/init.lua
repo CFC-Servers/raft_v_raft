c99a37b1c07424e9f722d66ba9206eb31047101e
Bleck <OverlordBleck@gmail.com>
2020-06-03 14:55:57 -0500

    Fixing module include position

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -8,10 +8,10 @@ include( "shared.lua" )
 include( "config/drowning.lua" )
 AddCSLuaFile( "config/drowning.lua" )
 
-include( "modules/hunger/sv_hunger.lua" )
-
 -- Modules
 include( "modules/drowning/sv_drowning.lua" )
 AddCSLuaFile( "modules/drowning/cl_drowning.lua" )
+
+include( "modules/hunger/sv_hunger.lua" )
 
 resource.AddFile( "materials/icons/health.png" )
