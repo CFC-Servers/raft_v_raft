d205b72e73700d29f5de3bc4cd814f9a8cf47d4e
Periapsises <45960263+Periapsises@users.noreply.github.com>
2020-06-04 14:13:40 -0400

    Add usergroups module

---
+++ gamemode/modules/usergroups/sh_usergroups.lua
@@ -0,0 +1,24 @@
+RVR_USER_ALL = 1
+RVR_USER_ADMIN = 2
+RVR_USER_SUPERADMIN = 3
+
+local userGroupNames = {
+    "user",
+    "admin",
+    "superadmin"
+}
+
+function RVR.getGroupName( userGroup )
+    return userGroupNames( userGroup )
+end
+
+function RVR.getUserGroup( ply )
+    if ply:IsSuperAdmin() then return RVR_USER_SUPERADMIN end
+    if ply:IsAdmin() then return RVR_USER_ADMIN end
+
+    return RVR_USER_ALL
+end
+
+function RVR.isUserGroup( ply, userGroup )
+    return RVR.getUserGroup( ply ) >= userGroup
+end
--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -2,11 +2,13 @@ GM.Name = "Raft v Raft"
 GM.Author = ""
 GM.Website = "https://github.com/cfc-servers/raft_v_raft"
 
+RVR = RVR or {}
+
 local function includeShared( f )
     AddCSLuaFile( f )
     include( f )
 end
 
-includeShared( "modules/commands/sh_commands.lua" )
+includeShared( "modules/usergroups/sh_usergroups.lua" )
 includeShared( "config/hunger.lua" )
 includeShared( "modules/hunger/sh_hunger.lua" )
