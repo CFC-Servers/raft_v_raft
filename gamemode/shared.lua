1e55c0c67f46467bc672f408ed8caff13993812b
Periapsises <45960263+Periapsises@users.noreply.github.com>
2020-06-08 19:25:15 -0400

    Declare RVR

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -2,6 +2,8 @@ GM.Name = "Raft v Raft"
 GM.Author = ""
 GM.Website = "https://github.com/cfc-servers/raft_v_raft"
 
+RVR = RVR or {}
+
 AddCSLuaFile( "load_config.lua" )
 AddCSLuaFile( "load_modules.lua" )
 include( "load_config.lua" )
