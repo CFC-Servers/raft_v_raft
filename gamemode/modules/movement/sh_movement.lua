9e946e23dd14c9c5a96ee3cc9c9016bc96b11623
PLally <11771225+PLally@users.noreply.github.com>
2020-06-09 19:19:48 +0000

    temporary custom move hook

--- gamemode/modules/movement/sh_movement.lua
+++ gamemode/modules/movement/sh_movement.lua
@@ -1,6 +1,17 @@
-function GM:Move( ply, mv )
+function GM:FinishMove( ply, mv )
     local ground = ply:GetGroundEntity()
     if not IsValid( ground ) or ground:GetClass() ~= "raft_foundation" then return end
-
-    -- TODO: custom movement on rafts maybe?
+    
+    local vel = mv:GetVelocity()
+    ply:SetLocalVelocity( vel )
+	ply:SetAbsVelocity( vel )
+	ply:SetLocalAngles( mv:GetAngles() )
+    
+    local pos = mv:GetOrigin() + vel * FrameTime() * 0.3
+    pos.z = math.min( 20, pos.z)
+    ply:SetPos( pos )
+    ply:SetLocalPos( pos )
+    ply:SetNetworkOrigin( pos )
+    
+    return true
 end
