56beced81f9a93e51e0368925bdb90999fdbc64d
Samuel Williams <33094920+samuelWilliams99@users.noreply.github.com>
2020-07-05 21:35:02 +0100

    Add Oxygen bar

--- gamemode/modules/drowning/cl_drowning.lua
+++ gamemode/modules/drowning/cl_drowning.lua
@@ -1,6 +1,9 @@
 local drownTimeStart
 local shouldDrawDrowningElement = false
 
+local barForeground = Material( "rvr/backgrounds/oxygen_bar_foreground.png" )
+local barBackground = Material( "rvr/backgrounds/oxygen_bar_background.png" )
+
 net.Receive( "RVR_Player_Enter_Water", function()
     local time = net.ReadFloat()
 
@@ -17,7 +20,28 @@ net.Receive( "RVR_Player_Take_Drown_Damage", function()
 end )
 
 local function renderDrowningElement()
-    -- TODO: Add HUD elements for drowning
+    if not shouldDrawDrowningElement then return end
+
+    local timePassed = CurTime() - drownTimeStart
+    local prog = timePassed / GAMEMODE.Config.Drowning.DROWNING_THRESHOLD
+
+    local hotbarFrame = RVR.Inventory.hotbar.frame
+    local x, y = hotbarFrame:GetPos()
+    local w = hotbarFrame:GetWide()
+    local wToRemove = 40
+
+    w = w - wToRemove
+    x = x + wToRemove * 0.5
+
+    local h = w * ( 58 / 808 ) * 0.6
+    y = y - h - 10
+
+    surface.SetDrawColor( Color( 255, 255, 255 ) )
+    surface.SetMaterial( barBackground )
+    surface.DrawTexturedRect( x, y, w, h )
+
+    surface.SetMaterial( barForeground )
+    surface.DrawTexturedRect( x + 2, y + 2, ( w - 4 ) * ( 1 - prog ), h - 4 )
 end
 
 hook.Add( "HUDPaint", "RVR_Render_Drowning_Element", renderDrowningElement )
