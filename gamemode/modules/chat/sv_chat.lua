28f45bace0ba893c2b68095d1478671346c58c6e
PLally <11771225+PLally@users.noreply.github.com>
2020-07-10 18:27:17 +0000

    config for 3d voice, add RVR.canHear function

--- gamemode/modules/chat/sv_chat.lua
+++ gamemode/modules/chat/sv_chat.lua
@@ -1,13 +1,27 @@
+function RVR.canHear( listener, speaker )
+    if not listener:Alive() or not speaker:Alive() then
+        return false
+    end
+    
+    local speakerPos = speaker:GetPos()
+    local listenerPos = listener:GetPos()
+
+    if listenerPos:Distance( speakerPos ) > GAMEMODE.Config.Chat.CHAT_DISTANCE then
+        return false
+    end
+    
+    return true
+end
+
 function GM:PlayerCanSeePlayersChat( text, teamOnly, listener, speaker )
     if teamOnly then
         -- TODO check party system?
         return false
     end
-    local listenerPos = listener:GetPos()
-    local speakerPos = speaker:GetPos()
-    if listenerPos:Distance( speakerPos ) > GAMEMODE.Config.Chat.CHAT_DISTANCE then
-        return false
-    end
+    
+    return RVR.canHear( listener, speaker )
+end
 
-    return true
+function GM:PlayerCanHearPlayersVoice( listener, speaker )
+    return RVR.canHear( listener, speaker ), GAMEMODE.Config.Chat.VOICE_3D
 end
