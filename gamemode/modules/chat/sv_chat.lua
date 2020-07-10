0553e46e7eea480ca08b7757d42648b35ecf4678
PLally <11771225+PLally@users.noreply.github.com>
2020-07-10 18:02:14 +0000

    get lister and  speaker pos

--- gamemode/modules/chat/sv_chat.lua
+++ gamemode/modules/chat/sv_chat.lua
@@ -3,7 +3,8 @@ function GM:PlayerCanSeePlayersChat( text, teamOnly, listener, speaker )
         -- TODO check party system?
         return false
     end
-    
+    local listenerPos = listener:GetPos()
+    local speakerPos = speaker:GetPos()
     if listenerPos:Distance( speakerPos ) > GAMEMODE.Config.Chat.CHAT_DISTANCE then
         return false
     end
