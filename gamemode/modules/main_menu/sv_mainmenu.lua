5eab6558358a797e7d21a94a5805154e7e0a86f5
Samuel Williams <33094920+samuelWilliams99@users.noreply.github.com>
2020-07-17 23:51:26 +0100

    Add spawning on menu close, fix bugs

--- gamemode/modules/main_menu/sv_mainmenu.lua
+++ gamemode/modules/main_menu/sv_mainmenu.lua
@@ -1,12 +1,20 @@
 util.AddNetworkString( "RVR_MainMenu_SetModel" )
+util.AddNetworkString( "RVR_MainMenu_SpawnSelf" )
 
 net.Receive( "RVR_MainMenu_SetModel", function( len, ply )
     local model = net.ReadString()
+    local models = GAMEMODE.Config.Generic.PLAYER_MODELS
 
-    if not table.HasValue( GAMEMODE.Config.Generic.PLAYER_MODELS, model ) then
+    if not table.HasValue( models, model ) then
         return
     end
 
     ply:SetModel( model )
     ply.PlayerModel = model
+end )
+
+net.Receive( "RVR_MainMenu_SpawnSelf", function( len, ply )
+    if not ply:Alive() then
+        ply:Spawn()
+    end
 end )
