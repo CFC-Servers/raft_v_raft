865f80877747122f12cdbc016f2d7cfc4b02316b
Samuel Williams <33094920+samuelWilliams99@users.noreply.github.com>
2020-07-20 00:39:18 +0100

    Change player model field, make spawnSelf net msg check RVR_PlayerCanSpawn

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -6,8 +6,8 @@ resource.AddFile( "resource/fonts/bungee_regular.ttf" )
 
 -- Needed to show holding animations, to be remove when main menu implemented
 function GM:PlayerSetModel( ply )
-    ply.PlayerModel = ply.PlayerModel or RVR.Config.Generic.DEFAULT_PLAYER_MODEL
-    ply:SetModel( ply.PlayerModel )
+    ply.RVR_PlayerModel = ply.RVR_PlayerModel or RVR.Config.Generic.DEFAULT_PLAYER_MODEL
+    ply:SetModel( ply.RVR_PlayerModel )
 end
 
 include( "load_assets.lua" )
