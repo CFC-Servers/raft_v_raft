d6378c9b3c197d1568ff03fbf35a878310d73222
PLally <11771225+PLally@users.noreply.github.com>
2020-07-26 17:17:54 +0000

    trash  in the ocean

---
+++ entities/entities/rvr_scrap_barrel/shared.lua
@@ -0,0 +1,7 @@
+ENT.Type = "anim"
+ENT.Base = "rvr_storage"
+ENT.PrintName = "Scrap Barrel"
+ENT.Author = ""
+ENT.Model = "models/props_c17/oildrum001.mdl"
+ENT.InventoryName = "Scrap barrel"
+ENT.InventorySize = 50
---
+++ gamemode/config/trash.lua
@@ -0,0 +1,19 @@
+GM.Config = GM.Config or {}
+
+GM.Config.Trash = {}
+local Config = GM.Config.Trash
+Config.SPAWN_RADIUS = 1000
+Config.SPAWN_WIDTH = 2000
+Config.MAX_TRASH_PER_PlAYER = 40
+Config.SPAWN_Z_OFFSET = -100
+Config.POSSIBLE_ENTITIES = {
+    { itemType = "wood", weight = 10, count = 10 },
+    { class = "rvr_scrap_barrel", weight = 2 },
+    { itemType = "nail", weight = 10, count = 10 }
+}
+
+Config.SCRAP_BARREL_ITEMS = {
+    { itemType = "wood", count = 10, weight = 10 },
+    { itemType = nil, weight = 40 },
+    { itemType = "nail", count = 15, weight = 10 }
+}
