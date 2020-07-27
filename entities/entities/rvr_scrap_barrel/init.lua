cca63d4c92708561035fa809ade5c2176f613466
Samuel Williams <33094920+samuelWilliams99@users.noreply.github.com>
2020-07-27 18:32:46 +0100

    Change barrels to break rather than be inventory

--- entities/entities/rvr_scrap_barrel/init.lua
+++ entities/entities/rvr_scrap_barrel/init.lua
@@ -3,23 +3,38 @@ AddCSLuaFile( "shared.lua" )
 
 include( "shared.lua" )
 
-function ENT:Initialize()
-    self.BaseClass.Initialize( self )
+local indexCounter = 0
 
-    -- TODO shared despawnable storage baseclass with death box
+function ENT:Initialize()
+    self:SetModel( self.Model )
+
     -- Set up despawn timer
-    local despawnTime = 300
+    local despawnTime = GAMEMODE.Config.Trash.BARREL_DESPAWN_TIME
     if despawnTime > 0 then
-        self.timerIdentifier = "rvr_scrap_barrel_despawn_" .. self:EntIndex()
+        self.timerIdentifier = "rvr_scrap_barrel_despawn_" .. indexCounter
+        indexCounter = indexCounter + 1
         local this = self
 
         timer.Create( this.timerIdentifier, despawnTime, 1, function()
-            if IsValid( this ) then this:Remove() end
+            if IsValid( this ) then
+                this.despawning = true
+                this:Remove()
+            end
         end )
     end
-    local phys = self:GetPhysicsObject()
-    if IsValid( phys ) then
-        phys:SetBuoyancyRatio( 0.7 )
+
+    self:PhysicsInit( SOLID_VPHYSICS )
+    self:SetMoveType( MOVETYPE_VPHYSICS )
+    self:SetSolid( SOLID_VPHYSICS )
+    self:SetUseType( SIMPLE_USE )
+    self:SetCollisionGroup( COLLISION_GROUP_WEAPON )
+
+    local physObj = self:GetPhysicsObject()
+
+    if physObj:IsValid() then
+        physObj:Wake()
+        physObj:EnableMotion( true )
+        physObj:SetBuoyancyRatio( 0.2 )
     end
 end
 
@@ -27,18 +42,23 @@ function ENT:OnRemove()
     if self.timerIdentifier then
         timer.Remove( self.timerIdentifier )
     end
+
+    if self.despawning then return end
+
+    for i, itemData in pairs( self.items ) do
+        local itemInstance = RVR.Items.getItemInstance( itemData.itemType )
+
+        local droppedItem = ents.Create( "rvr_dropped_item" )
+        droppedItem:Setup( itemInstance, itemData.count )
+        droppedItem:SetPos( self:GetPos() + Vector( math.Rand( -5, 5 ), math.Rand( -5, 5 ), math.Rand( -5, 5 ) ) )
+        droppedItem:Spawn()
+    end
+end
+
+function ENT:OnTakeDamage()
+    self:Remove()
 end
 
 function ENT:SetItems( items )
-    for i = 1, #items  do
-        local item = items[i]
-        if item and item.itemType then
-            RVR.Inventory.setSlot( self, i, { 
-                item = RVR.Items.getItemData( item.itemType ), 
-                count = item.count 
-            } )
-        end
-    end
- 
-    self.RVR_Inventory.PreventAdding = true   
+    self.items = items
 end
--- entities/entities/rvr_scrap_barrel/shared.lua
+++ entities/entities/rvr_scrap_barrel/shared.lua
@@ -1,7 +1,5 @@
 ENT.Type = "anim"
-ENT.Base = "rvr_storage"
+ENT.Base = "base_entity"
 ENT.PrintName = "Scrap Barrel"
 ENT.Author = ""
 ENT.Model = "models/props_c17/oildrum001.mdl"
-ENT.InventoryName = "Scrap barrel"
-ENT.InventorySize = 50
