23fbd2367bf64bc208033cd42c81aed532f46250
PLally <11771225+PLally@users.noreply.github.com>
2020-07-27 17:21:49 +0000

    random counts for items

--- gamemode/config/trash.lua
+++ gamemode/config/trash.lua
@@ -7,13 +7,13 @@ Config.SPAWN_WIDTH = 2000
 Config.MAX_TRASH_PER_PlAYER = 40
 Config.SPAWN_Z_OFFSET = -100
 Config.POSSIBLE_ITEMS = {
-    { itemType = "wood", weight = 10, count = 10 },
+    { itemType = "wood", weight = 10, mincount = 5, maxcount = 15 },
     { class = "rvr_scrap_barrel", weight = 2 },
-    { itemType = "nail", weight = 10, count = 10 }
+    { itemType = "nail", weight = 5, mincount = 5, maxcount = 15 }
 }
 
 Config.SCRAP_BARREL_ITEMS = {
-    { itemType = "wood", count = 10, weight = 10 },
-    { itemType = nil, weight = 40 },
-    { itemType = "nail", count = 15, weight = 10 }
+    { itemType = "wood", weight = 10, mincount = 2, maxcount = 10 },
+    { itemType = nil, weight = 30 },
+    { itemType = "nail", weight = 10, mincount = 5, maxcount = 20 }
 }
