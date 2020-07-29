1201e56b11c65cfc80545d8fa683107423714eee
PLally <11771225+PLally@users.noreply.github.com>
2020-07-29 22:09:00 +0000

    implement max fish cap, use timer instead of tick hook

--- gamemode/config/fish.lua
+++ gamemode/config/fish.lua
@@ -2,10 +2,13 @@ GM.Config = GM.Config or {}
 GM.Config.Fish = GM.Config.Fish or {}
 
 local config = GM.Config.Fish
-config.tickInterval = 4000 -- How often fish have the chance to spawn in miliseconds
-config.spawnRadius = 500
-config.waterLevelBias = 20
-config.fish = {
+config.FISH_TIMER_DELAY = 10 -- How often fish have the chance to spawn in seconds
+config.SPAWN_RADIUS = 1000
+config.DESPAWN_RADIUS = 1500
+config.WATER_LEVEL_BIAS = 20
+config.FISH_PER_PLAYER = 10
+
+config.FISH = {
     {
         type = "tuna",
         item = "tuna",
