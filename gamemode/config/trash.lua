1bee56deb2a8a2ace23c05529427a284b2ca8615
PLally <11771225+PLally@users.noreply.github.com>
2020-07-30 04:47:49 +0000

    POSSIBLE_ITEMS item definitions on multiple lines

--- gamemode/config/trash.lua
+++ gamemode/config/trash.lua
@@ -10,9 +10,22 @@ Config.BARREL_DESPAWN_TIME = 300
 Config.SCRAP_BARREL_ITEM_AMOUNT = 5
 
 Config.POSSIBLE_ITEMS = {
-    { itemType = "wood", weight = 10, minCount = 5, maxCount = 15 },
-    { class = "rvr_scrap_barrel", weight = 2 },
-    { itemType = "nail", weight = 5, minCount = 5, maxCount = 15 }
+    {
+        itemType = "wood",
+        weight = 10,
+        minCount = 5,
+        maxCount = 15
+    },
+    {
+        class = "rvr_scrap_barrel",
+        weight = 2
+    },
+    {
+        itemType = "nail",
+        weight = 5,
+        minCount = 5,
+        maxCount = 15
+    }
 }
 
 Config.SCRAP_BARREL_ITEMS = {
