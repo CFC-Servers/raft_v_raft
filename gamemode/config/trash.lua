468372407e61cd81c926ce69d0691cc3e7d3e739
Bleck <OverlordBleck@gmail.com>
2020-07-31 13:00:38 -0500

    Adjusting trash spawn rates and items

--- gamemode/config/trash.lua
+++ gamemode/config/trash.lua
@@ -3,32 +3,61 @@ GM.Config = GM.Config or {}
 GM.Config.Trash = {}
 local Config = GM.Config.Trash
 Config.SPAWN_RADIUS = 1000
-Config.SPAWN_WIDTH = 2000
-Config.MAX_TRASH_PER_PlAYER = 40
+Config.SPAWN_WIDTH = 1500
+Config.MAX_TRASH_PER_PlAYER = 15
 Config.SPAWN_Z_OFFSET = -100
-Config.BARREL_DESPAWN_TIME = 300
+Config.BARREL_DESPAWN_TIME = 120
 Config.SCRAP_BARREL_ITEM_AMOUNT = 5
 
 Config.POSSIBLE_ITEMS = {
     {
         itemType = "wood",
         weight = 10,
-        minCount = 5,
-        maxCount = 15
+        minCount = 1,
+        maxCount = 5
     },
     {
         class = "rvr_scrap_barrel",
         weight = 1
     },
     {
-        itemType = "nail",
-        weight = 5,
-        minCount = 5,
-        maxCount = 15
+        weight = 3
     }
 }
 
 Config.SCRAP_BARREL_ITEMS = {
-    { itemType = "wood", weight = 10, minCount = 2, maxCount = 10 },
-    { itemType = "nail", weight = 10, minCount = 5, maxCount = 20 }
+    {
+        itemType = "wood",
+        weight = 5,
+        minCount = 5,
+        maxCount = 15
+    },
+    {
+        itemType = "nail",
+        weight = 5,
+        minCount = 5,
+        maxCount = 10
+    },
+    {
+        itemType = "dirty_water",
+        weight = 5
+    },
+    {
+        itemType = "scrap_metal",
+        weight = 3,
+        minCount = 1,
+        maxCount = 5
+    },
+    {
+        itemType = "cloth",
+        weight = 3,
+        minCount = 1,
+        maxCount = 5
+    },
+    {
+        itemType = "small_rocks",
+        weight = 3,
+        minCount = 1,
+        maxCount = 5
+    }
 }
