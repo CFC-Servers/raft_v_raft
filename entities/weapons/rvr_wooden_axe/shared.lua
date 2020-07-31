82624a5958654dfbdd3b6023c6155e28fc885905
PLally <11771225+PLally@users.noreply.github.com>
2020-07-31 15:13:27 -0400

    make axes do less damage

--- entities/weapons/rvr_wooden_axe/shared.lua
+++ entities/weapons/rvr_wooden_axe/shared.lua
@@ -4,3 +4,4 @@ SWEP.PrintName = "Wooden Axe"
 SWEP.Base = "rvr_melee_base"
 SWEP.ViewModel = "models/rvr/items/wooden_axe.mdl"
 SWEP.WorldModel = "models/rvr/items/wooden_axe.mdl"
+SWEP.Primary.Damage = 10
