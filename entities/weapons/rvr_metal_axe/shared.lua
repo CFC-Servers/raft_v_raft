0c869e4618b84e6f66b45c59e9098656085f078f
PLally <11771225+PLally@users.noreply.github.com>
2020-07-31 21:59:25 +0000

    add metal spear, other stuff, idk

---
+++ entities/weapons/rvr_metal_axe/shared.lua
@@ -0,0 +1,7 @@
+AddCSLuaFile()
+
+SWEP.PrintName = "Metal Axe"
+SWEP.Base = "rvr_wooden_axe"
+SWEP.ViewModel = "models/rvr/items/metal_axe.mdl"
+SWEP.WorldModel = "models/rvr/items/metal_axe.mdl"
+SWEP.Primary.Damage = 45
